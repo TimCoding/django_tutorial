f2ed7a3aa9700777f63eacfaab97207004090e8a
TimCoding <timbvho@gmail.com>
2017-05-20 21:22:02 -0500

    models

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'plant.apps.PlantConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- plant/models.py
+++ plant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    t_name = models.CharField(max_length=250)
+    t_img = models.CharField(max_length=1000)
+    t_description = models.CharField(max_length=1000)
+
+class Plant(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    p_name = models.CharField(max_length=200)
+    p_img = models.CharField(max_length=200)
+    p_description = models.CharField(max_length=1000)
+    p_quantity = models.IntegerField()
+
