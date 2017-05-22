80e734421514ac8a54b926760841e75aa691bc0b
TimCoding <timbvho@gmail.com>
2017-05-22 18:25:23 -0500

    Models updated, generate slugs

--- plant/models.py
+++ plant/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Type(models.Model):
     t_name = models.CharField(max_length=250)
     t_img = models.CharField(max_length=1000)
     t_description = models.CharField(max_length=1000)
+    slug = models.SlugField(default='')
+
+    def save(self, *args, **kwargs):
+        super(Type, self).save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.t_name) + "-" + str(self.id)
+            self.save()
 
     def __str__(self):
         return self.t_name
@@ -15,4 +23,11 @@ class Plant(models.Model):
     p_img = models.CharField(max_length=200)
     p_description = models.CharField(max_length=1000)
     p_quantity = models.IntegerField()
+    slug = models.SlugField(default='')
+
+    def save(self, *args, **kwargs):
+        super(Plant, self).save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.p_name) + "-" + str(self.id)
+            self.save()
 
