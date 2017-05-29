34376ebaed36b2e54db2510e6465825e541a5bbd
TimCoding <timbvho@gmail.com>
2017-05-29 16:59:06 -0500

    Added base template

--- plant/views.py
+++ plant/views.py
@@ -26,21 +26,3 @@ def plant(request, type, plant_id, slug):
     #Actually selects plant to be used
     selected_plant = get_object_or_404(Plant, pk=plant_id)
     return render(request, 'plant/plant.html', {'plant': selected_plant})
-
-def favorite(request, type, plant_id, slug):
-    #Checks to see if the type of plant is still correct
-    selected_type = get_object_or_404(Type, t_name=type)
-    #Actually selects plant to be used
-    try:
-        selected_plant = get_object_or_404(Plant, pk=request.POST['plant'])
-    except (KeyError, Plant.DoesNotExist):
-        return render(request, 'plant/detail.html', {
-            'type' : selected_type,
-            'plants' : selected_type.plant_set.all(),
-            'error_message' : "You did not select a valid plant",
-        })
-    else: 
-        is_favorited = selected_plant.p_favorite
-        selected_plant.p_favorite = not(is_favorited)
-        selected_plant.save()
-        return render(request, 'plant/plant.html', {'plant': selected_plant})
